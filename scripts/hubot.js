94f56d9f9832b94e130853923f837c0d2d30b513
wearle <wearle@paintnite.com>
2016-07-13 20:14:11 -0400

    changes

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -22,5 +22,9 @@ robot.hear(/Hello!/, function(res) {
 	return res.send("Hi there!");
 });
 
+robot.respond(/What's your favorite food?/, function(res) {
+	return res.send("I'm a robot--I don't eat food!");
+});
+
 
 }
