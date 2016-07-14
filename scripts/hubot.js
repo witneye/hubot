2360eb29942940b4b80c7aa1a09d06df4fb00fe7
wearle <wearle@paintnite.com>
2016-07-13 20:12:12 -0400

    changes

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -2,21 +2,25 @@ module.exports = function(robot) {
 
 robot.respond(/is it a (weekend|holiday)\s?\?/i, function(msg){
 var today = new Date();
-
-msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
+	msg.reply(today.getDay() === 0 || today.getDay() === 6 ? "YES" : "NO");
 });
 
 robot.hear(/i did it/i, function(msg){
-msg.send("Congratulations! Good job!");
+	msg.send("Congratulations! Good job!");
 });
 
 robot.respond(/are you there?/i, function(msg){
-msg.reply('Yes, usually here, and listening.');
+	msg.reply('Yes, usually here, and listening.');
 });
 
 robot.respond(/convert \$(.*) to btc/i, function(res){
 var usd = res.match[1];
-res.reply('That is about ' + usd * 0.0024 + ' in BTC');
+	res.reply('That is about ' + usd * 0.0024 + ' in BTC');
 });
+
+robot.hear(/Hello!/, function(res) {
+	return res.send("Hi there!");
+});
+
 
 }
