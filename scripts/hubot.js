8ab443c3b929f8958b1f559526a78f9b7d417e7c
wearle <wearle@paintnite.com>
2016-07-18 19:13:36 -0400

    added second function

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -4,5 +4,11 @@ robot.respond(/What do you eat?/, function(res) {
 	return res.send("I'm a robot--I don't eat!");
 });
 
+robot.respond(/Do we have class today?/i, function(msg){
+var today = new Date();
+
+msg.reply(today.getDay() === 1 || today.getDay() === 3 ? "YES" : "NO");
+});
+
 
 }
