43c45e2168db150e2291881cf45832a4e2625d36
wearle <wearle@paintnite.com>
2016-07-18 21:23:51 -0400

    added second function

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -6,43 +6,12 @@ module.exports = function(bot) {
 
 	bot.respond(/Is class meeting today?/i, function(msg){
 		var today = new Date();
-		msg.reply(today.getDay() === 1 || today.getDay() === 3 ? "Yes we do, because today is " + today : "nah, not today");
-	});
-
-	bot.hear(/Yo/, function(res) {
-		return res.send(res.message.user.name, 'hi');
+		msg.reply(today.getDay() === 2 || today.getDay() === 3 ? "Yes we do, because today is " + today : "nah, not today");
 	});
 
 	bot.hear(/oh nelly/i, function(res) {
     	bot.messageRoom(res.message.user.name, "awesome!");
   	});
 
-
-
-	// bot.respond(/oh nelly/i, function (msg) {
- //    	bot.send({ user: msg.message.user.jid }, 'hi');
- //  	});
-
-// bot.respond(/oh nelly/i, function (msg) {
-// 	msg.envelope.user.type = 'direct'
-//   	msg.send "Hey #{msg.envelope.user.name}!"
-// });
-
- //    bot.hear(/hey/, function(msg) {
- //    	return msg.envelope.user.id.send("Well, Hello there");
-	// });
-
-	
-
-	// robot.hear(['hello'], 'ambient', function(bot, message) {
-	//   username = user.info.name;
-	//   bot.reply(message, username);
-	// });
-
-	// controller.hears(/hey/, function(bot, message) {
- //  		bot.api.users.info({user: message.user}, function(err, info){
- //    	bot.reply(message, info.user.name)
- //  		});
-	// });
 
 }
