51f9ed5d75d23da3ea70376acbf9071a262757e6
wearle <wearle@paintnite.com>
2016-07-18 21:34:31 -0400

    added second function

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -5,8 +5,9 @@ module.exports = function(bot) {
 	});
 
 	bot.respond(/Is class meeting today?/i, function(msg){
-		var today = new Date();
-		msg.reply(today.getDay() === 2 || today.getDay() === 3 ? "Yes we do, because today is " + today : "nah, not today");
+		var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+		var day = days[ now.getDay() ];
+		msg.reply(today.getDay() === 2 || today.getDay() === 3 ? "Yes we do, because today is " + day : "nah, not today is " + day);
 	});
 
 	bot.hear(/oh nelly/i, function(res) {
