fad956ec898ea2296991a236510db0ebb6fd7267
wearle <wearle@paintnite.com>
2016-07-18 20:21:33 -0400

    added second function

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -1,18 +1,32 @@
 module.exports = function(robot) {
 
-robot.respond(/What do you eat?/, function(res) {
-	return res.send("I'm a robot--I don't eat!");
-});
+	robot.respond(/What do you eat?/, function(res) {
+		return res.send("I'm a robot--I don't eat!");
+	});
 
-robot.respond(/Is class meeting today?/i, function(msg){
-	var today = new Date();
-	msg.reply(today.getDay() === 1 || today.getDay() === 3 ? "Yes we do, because today is " + today : "nah, not today");
-});
+	robot.respond(/Is class meeting today?/i, function(msg){
+		var today = new Date();
+		msg.reply(today.getDay() === 1 || today.getDay() === 3 ? "Yes we do, because today is " + today : "nah, not today");
+	});
 
-robot.hears(/'Hello There!'/, function(bot, message) {
-  username = users.info.name;
-  bot.reply(message, username);
-});
+	robot.hear(/Yo/, function(res) {
+		return res.send(res.message.user.name);
+	});
+
+	
+
+
+	// robot.hear(['hello'], 'ambient', function(bot, message) {
+	//   username = user.info.name;
+	//   bot.reply(message, username);
+	// });
+
+	// controller.hears(['texas'], 'ambient', function(bot, message) {
+ //  		bot.api.users.info({user: message.user}, function(err, info){
+    	
+ //    	bot.reply(message, info.user.name)
+ //  		});
+	// })
 
 
 }
