c531734491eaaffade65c01df7473a483d8f7125
wearle <wearle@paintnite.com>
2016-07-18 20:33:00 -0400

    added second function

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -14,7 +14,7 @@ module.exports = function(robot) {
 	});
 
 	robot.hear(/hey/, function(res) {
-    	res.message.user.name.reply("Well, Hello there");
+    	res.message.user.name.send("Well, Hello there");
 	});
 
 	
