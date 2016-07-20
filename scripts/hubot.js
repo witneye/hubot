5c5f98abf4154a784a004ca8f7f1016808831b03
wearle <wearle@paintnite.com>
2016-07-19 22:24:39 -0400

    Initial Commit

--- scripts/hubot.js
+++ scripts/hubot.js
@@ -5,9 +5,20 @@ module.exports = function(bot) {
 	});
 
 	bot.respond(/Is class meeting today?/i, function(msg){
-		var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-		var day = days[ now.getDay() ];
-		msg.reply(today.getDay() === 2 || today.getDay() === 3 ? "Yes we do, because today is " + day : "nah, not today is " + day);
+		
+		var weekdays = new Array(7);
+		weekdays[0] = "Sunday";
+		weekdays[1] = "Monday";
+		weekdays[2] = "Tuesday";
+		weekdays[3] = "Wednesday";
+		weekdays[4] = "Thursday";
+		weekdays[5] = "Friday";
+		weekdays[6] = "Saturday";
+
+		var today = new Date();
+		day = today.getDay();
+		
+		msg.reply(today.getDay() === 2 || today.getDay() === 3 ? "Yes we do, because today is " + weekdays[day] : "nah, not today is " + weekdays[day]);
 	});
 
 	bot.hear(/oh nelly/i, function(res) {
